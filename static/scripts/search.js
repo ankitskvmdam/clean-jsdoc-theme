bea349ace51f29dbe620ae5fa8e941d5d2e0bea3
Ankit Kumar <hello@ankdev.me>
2020-10-16 21:40:31 +0530

    lint js

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,42 +1,24 @@
-
-function hideSearchList(){
-  document.getElementById("search-item-ul").style.display = "none";
+/* global document */
+function hideSearchList() {
+    document.getElementById('search-item-ul').style.display = 'none';
 }
 
-function showSearchList(){  
-  document.getElementById("search-item-ul").style.display = "block";
+function showSearchList() {
+    document.getElementById('search-item-ul').style.display = 'block';
 }
 
-function checkClick(e){
-  if( e.target.id != "search-box"){
-    this.setTimeout(function(){
-      hideSearchList()
-    }, 60);
-    
-    window.removeEventListener('click', checkClick)
-  }
-}
+function checkClick(e) {
+    if ( e.target.id !== 'search-box') {
+        setTimeout(function() {
+            hideSearchList();
+        }, 60);
 
-function setupSearch(){
-  var input_box = document.getElementById("search-box");
-  var keys = ["title"]
-
-  input_box.addEventListener('keyup', function(){
-    if(input_box.value != ""){ 
-      showSearchList();
-      search(list, keys, input_box.value)
+        /* eslint-disable-next-line */
+        window.removeEventListener('click', checkClick);
     }
-    else hideSearchList();
-  })
-
-  input_box.addEventListener('focus', function(){
-    showSearchList();
-    if(input_box.value != "") search(list, keys, input_box.value)
-    window.addEventListener("click", checkClick)
-  })
 }
 
-function search(list, keys, search_key){
+function search(list, keys, searchKey) {
     var options = {
         shouldSort: true,
         threshold: 0.4,
@@ -45,18 +27,46 @@ function search(list, keys, search_key){
         maxPatternLength: 32,
         minMatchCharLength: 1,
         keys: keys
-      };
-      var fuse = new Fuse(list, options);
-      var result = fuse.search(search_key);
-      var search = document.getElementById("search-item-ul")
-      
-      search.innerHTML = "";
+    };
 
-      if (result.length == 0) {
-        search.innerHTML+="<li> No Result Found </li>";
-      } else {
-        result.forEach(function(item){
-          search.innerHTML+="<li>"+item.link+"</li>";
+    /* eslint-disable-next-line */
+    var fuse = new Fuse(list, options);
+    var result = fuse.search(searchKey);
+    var searchUL = document.getElementById('search-item-ul');
+
+    searchUL.innerHTML = '';
+
+    if (result.length === 0) {
+        searchUL.innerHTML += '<li> No Result Found </li>';
+    } else {
+        result.forEach(function(item) {
+            searchUL.innerHTML += '<li>' + item.link + '</li>';
         });
-      }
+    }
 }
+
+/* eslint-disable-next-line */
+function setupSearch(list) {
+    var inputBox = document.getElementById('search-box');
+    var keys = ['title'];
+
+    inputBox.addEventListener('keyup', function() {
+        if (inputBox.value !== '') {
+            showSearchList();
+            search(list, keys, inputBox.value);
+        }
+        else { hideSearchList(); }
+    });
+
+    inputBox.addEventListener('focus', function() {
+        showSearchList();
+        if (inputBox.value !== '') {
+            search(list, keys, inputBox.value);
+        }
+
+        /* eslint-disable-next-line */
+        window.addEventListener('click', checkClick);
+    });
+}
+
+
