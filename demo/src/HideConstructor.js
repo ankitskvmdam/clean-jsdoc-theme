b7ffc1660d7b9abd29de051e1c4d7e4482c27ea7
Ankit Kumar <hello@ankdev.me>
2022-03-13 13:34:34 +0000

    add test file to check hide constructor

---
+++ demo/src/HideConstructor.js
@@ -0,0 +1,17 @@
+/**
+ * Constructor should be hidden
+ * @hideconstructor
+ */
+class Test {
+	constructor(hidemepls) {
+		console.log('i am a constructor');
+    }
+    
+    /**
+     * 
+     * @param {TestConfigObject} config 
+     */
+    start(config) {
+
+    }
+}
