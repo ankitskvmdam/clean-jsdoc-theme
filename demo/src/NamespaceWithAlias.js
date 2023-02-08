c2fa52134161b318ead8b56d6573216a3eb429e6
Ankit Kumar <ankits.kvmdam@hotmail.com>
2023-02-08 10:35:13 +0900

    add alias

---
+++ demo/src/NamespaceWithAlias.js
@@ -0,0 +1,74 @@
+function create_public_interface(obj) {
+    /**
+     * @namespace obj.map
+     */
+    const map = obj.map;
+
+    /**
+     * Sample function that does nothing
+     * @memberOf obj.map
+     * @function
+     */
+    map.start = function () {}
+    /**
+     * This is an alias. It's original name is start9
+     * @memberOf obj.map
+     * @function
+     * @alias obj.map.aliasOfStart9
+     */
+    map.start9 = function () {}
+    /**
+     * Sample function that does nothing
+     * @memberOf obj.map
+     * @function
+     */
+    map.start1 = function () {}
+    /**
+     * Sample function that does nothing
+     * @memberOf obj.map
+     * @function
+     */
+    map.start2 = function () {}
+    /**
+     * Sample function that does nothing
+     * @memberOf obj.map
+     * @function
+     */
+    map.start3 = function () {}
+    /**
+     * Sample function that does nothing
+     * @memberOf obj.map
+     * @function
+     */
+    map.start4 = function () {}
+    /**
+     * Sample function that does nothing
+     * @memberOf obj.map
+     * @function
+     */
+    map.start5 = function () {}
+    /**
+     * Sample function that does nothing
+     * @memberOf obj.map
+     * @function
+     */
+    map.start6 = function () {}
+    /**
+     * Sample function that does nothing
+     * @memberOf obj.map
+     * @function
+     */
+    map.start7 = function () {}
+    /**
+     * Sample function that does nothing
+     * @memberOf obj.map
+     * @function
+     */
+    map.start8 = function () {}
+    /**
+     * Sample function that does nothing
+     * @memberOf obj.map
+     * @function
+     */
+    map.start0 = function () {}
+}
