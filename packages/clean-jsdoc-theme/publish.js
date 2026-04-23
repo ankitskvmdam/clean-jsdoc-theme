813ef345e27fc3c180896f6722653257596546be
Ankit Kumar <ankits.kvmdam@hotmail.com>
2026-04-23 22:50:01 +0900

    working on the flow

--- packages/clean-jsdoc-theme/publish.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-// JSDoc resolves templates via require(templateDir + '/publish'), bypassing
-// package.json main/exports. This file proxies to the compiled output.
-module.exports = require('./dist/publish.js');
--- packages/clean-jsdoc-theme/src/publish.ts
+++ packages/clean-jsdoc-theme/src/publish.ts
@@ -1,8 +1,14 @@
 /* eslint-disable */
 'use strict';
+// import fs from 'node:fs';
+import { generateMdx } from '@clean-jsdoc-theme/setu';
 
 export function publish(data: any, opts: any, tutorials: any) {
-  console.log('[clean-jsdoc-theme v5] opts.destination:', opts && Object.keys(opts));
-  console.log('[clean-jsdoc-theme v5] data:', data());
-  console.log('[clean-jsdoc-theme v5] tutorials:', tutorials);
+  // Writing everything on disk.
+  // We will use it to do testing.
+  // const d = data();
+  // fs.writeFileSync('./data.json', JSON.stringify(d, null, 2));
+  // fs.writeFileSync('./opts.json', JSON.stringify(opts, null, 2));
+  // fs.writeFileSync('./tutorials.json', JSON.stringify(tutorials, null, 2));
+  generateMdx(data);
 }
