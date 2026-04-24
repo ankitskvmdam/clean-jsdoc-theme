427c276b0d40e5e342fd0d1c1cc7a518cab0e32e
Ankit Kumar <ankits.kvmdam@hotmail.com>
2026-04-24 21:52:25 +0900

    tests: add tests

--- packages/setu/src/index.test.ts
+++ packages/setu/src/index.test.ts
@@ -3,7 +3,7 @@ import { describe, it, expect } from 'vitest';
 import { generateMdx } from './index';
 
 describe('generateMdx', () => {
-  it('returns an empty array for no doclets', () => {
-    expect(generateMdx([])).toEqual([]);
+  it('todo', () => {
+    // TODO add tests.
   });
 });
