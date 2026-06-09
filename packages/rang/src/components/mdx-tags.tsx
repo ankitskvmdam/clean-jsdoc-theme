92d9ce232b7f5d379c97ec91fa9679798c7d04e8
Ankit Kumar <ankits.kvmdam@hotmail.com>
2026-06-09 17:26:23 +0900

    fix: generate new css

--- packages/rang/src/components/mdx-tags.tsx
+++ packages/rang/src/components/mdx-tags.tsx
@@ -25,7 +25,7 @@ export function MdxH1({ id, children, ...rest }: HeadingProps) {
   );
   if (!slot) return heading;
   return (
-    <HeaderRow marginClass="[&_h1]:mb-4" slot={slot}>
+    <HeaderRow marginClass="[&_h1]:m-0 mb-4" slot={slot}>
       {heading}
     </HeaderRow>
   );
